fede38afd184a9f2c06710db6e9f02c69e3b0657
JinneeJ <julia.pustovoytova@gmail.com>
2021-12-23 17:44:51 +0300

    Add publish action

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,7 +10,7 @@ repositories {
     mavenCentral()
 }
 group = "com.doist.gradle"
-version = "1.0.0"
+version = property("version") as String
 
 dependencies {
     compileOnly("org.jetbrains.kotlin:kotlin-gradle-plugin")
